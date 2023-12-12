3e359ce2af556792d0b62b6f464c36ec11c65421
AMV1909 <amorales264@unab.edu.co>
2023-12-12 14:37:02 -0500

    Updated db.js

--- src/database/db.js
+++ src/database/db.js
@@ -3,6 +3,10 @@ import { config } from "dotenv";
 
 config();
 
-export const connectDB = await connect(process.env.MONGODB_URI)
+export const connectDB = await connect(process.env.MONGODB_URI, {
+    dbName: process.env.MONGODB_NAME,
+})
     .then(() => console.log("Database connected"))
-    .catch((err) => console.log(err));
+    .catch((err) => {
+        throw new Error(err);
+    });
